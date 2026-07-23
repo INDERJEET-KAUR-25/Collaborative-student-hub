d563f0af77fc5d3e0b40bef443f07c49649b91b4
Inderjeet Kaur <ikaur1108@gmail.com>
2026-07-23 21:53:25 +0530

    Added core database models for projects

---
+++ projects/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+class Project(models.Model):
+    DIFFICULTY_CHOICES=[
+        ('Beginner','Beginner'),
+        ('Intermediate','Intermediate'),
+        ('Advanced','Advanced'),
+    ]
+
+    STATUS_CHOICES=[
+        ('Open','Open'),
+        ('In Progress','In Progress'),
+        ('Completed','Completed'),
+    ]
+
+    # Columns for project model
+
+    title=models.CharField(max_length=300)
+    description=models.TextField()
+    owner=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='owned_projects')
+    department=models.CharField(max_length=150)
+    deadline=models.DateField()
+    status=models.CharField(max_length=20,default='Open',choices=STATUS_CHOICES)
+    difficulty=models.CharField(max_length=20,default='Beginner',choices=DIFFICULTY_CHOICES)
+    team_size=models.PositiveIntegerField(default=1)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class ProjectSkills(models.Model):
+    project=models.ForeignKey(Project,on_delete=models.CASCADE,related_name='required_skills')
+
+    # Update 'skills.Skill' if Member 1 named their skill app/model differently
+    skill = models.ForeignKey('skills.Skill', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('project', 'skill')

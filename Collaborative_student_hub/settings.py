c72be4f1911854fb8480b0e91ffe9766da8611a3
Inderjeet Kaur <ikaur1108@gmail.com>
2026-07-23 22:05:23 +0530

    Made migrations for all models for each module andregistered apps in settings.py

--- Collaborative_student_hub/settings.py
+++ Collaborative_student_hub/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'projects',
+    'applications',
+    'teams',
+    'tasks',
+    'notifications'
 ]
 
 MIDDLEWARE = [

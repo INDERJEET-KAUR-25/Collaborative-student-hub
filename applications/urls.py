bc3320dd2d79ea3e720adbe7e947a9f5a11a28d7
Inderjeet Kaur <ikaur1108@gmail.com>
2026-07-27 14:49:05 +0530

    updated urls

--- applications/urls.py
+++ applications/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .import views
 
-urlspatterns=[
+urlpatterns=[
     path('applications/',views.application_list_create,name='application_list_create'),
     path('applications/<int:pk>/',views.application_detail_update,name='application_detail_update'),
 ]

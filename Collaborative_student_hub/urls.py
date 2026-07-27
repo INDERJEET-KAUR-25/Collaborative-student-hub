12ee523ef5b9bafc4fb604ad89ee0301f012111c
Inderjeet Kaur <ikaur1108@gmail.com>
2026-07-27 15:13:08 +0530

    included url for teams app

--- Collaborative_student_hub/urls.py
+++ Collaborative_student_hub/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('api/applications/',include('applications.urls')),
     path('api/tasks/',include('tasks.urls')),
     path('api/notifications/',include('notifications.urls')),
+    path('api/teams/',include('teams.urls')),
 ]

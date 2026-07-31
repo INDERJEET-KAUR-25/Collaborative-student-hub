25d62ef53e7c531215ccfcae7e716521329b178e
{rudhar} <{rudhar.guleria005@gmail.com}>
2026-07-31 09:36:03 +0530

    Fix CORS issue

--- Collaborative_student_hub/settings.py
+++ Collaborative_student_hub/settings.py
@@ -43,12 +43,14 @@ INSTALLED_APPS = [
     'applications',
     'teams',
     'tasks',
-    'notifications'
+    'notifications',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -143,3 +145,6 @@ SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
 }
+
+# Allow React Frontend to connect to Django API
+CORS_ALLOW_ALL_ORIGINS = True
